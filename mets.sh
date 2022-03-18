5df5ec45891b448ec07e06615e8e0d9fbaa5e4e9
tboenig <tboenig@users.noreply.github.com>
2022-03-18 18:57:45 +0000

    Deploying to gh-pages from  @ 22abb78d069e8dbcdd3b11f79ad4e2bd773896ff 🚀

--- mets.sh
+++
@@ -1,5 +0,0 @@
-ocrd workspace init Stimmen_aus_Maria-Laach/1872;
-            ocrd workspace init Stimmen_aus_Maria-Laach/1871;
-            ocrd workspace init Allgemeine_kirchliche_Zeitschrift/1860;
-            ocrd workspace init Die_paepstliche_Unfehlbarkeit;
-            
